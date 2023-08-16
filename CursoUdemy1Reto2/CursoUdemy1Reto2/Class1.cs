ff475d76ffe4f037c434def6d6995df542aaf30c
nicolasCaceresCragaris <116316867+nicolasCaceresCragaris@users.noreply.github.com>
2023-08-16 17:53:43 -0300

    Add files via upload

---
+++ CursoUdemy1Reto2/CursoUdemy1Reto2/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoUdemy1Reto2
+{
+    internal class Persona
+    {
+        public String Nombre { get; set; }
+
+        public String Apellido { get; set; }
+
+        public int Edad { get;set; }
+
+        public Persona(string nombre) 
+        {
+            Nombre = nombre;        
+        }
+        public Persona(string nombre,string apellido): this(nombre) 
+        {
+            Apellido = apellido;
+        }
+
+        public Persona(string nombre,string apellido, int edad):this(nombre,apellido)
+        { 
+            Edad = edad;    
+        }
+
+        public void Saludar()
+        {
+            Console.WriteLine("{0} {1}", Nombre, Apellido);
+            return;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Nombre:{0} Apellido:{1} Edad:{2}",Nombre, Apellido,Edad);
+        }
+
+    }
+}
---
+++ CursoUdemy1Reto2/CursoUdemy1Reto2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoUdemy1Reto2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona personaUno = new Persona("Eduardo", "Alfaro", 30);
+            Persona personaDos = new Persona("German", "Cano", 40);
+
+
+
+            personaUno.Saludar();
+            personaDos.Saludar();
+
+            Console.WriteLine(personaUno.ToString());
+            Console.WriteLine(personaDos.ToString());
+        }
+
+    }
+}
