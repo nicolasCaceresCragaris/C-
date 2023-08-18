2f93d2602c1ac6289f5b0927def5c15f87ad1841
nicolasCaceresCragaris <116316867+nicolasCaceresCragaris@users.noreply.github.com>
2023-08-17 21:17:46 -0300

    Add files via upload

---
+++ ejercicioCoches/ejercicioCoches/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicioCoches
+{
+    internal class Coche
+    {
+        private int Id { get; set; }
+        private string Marca { get; set; }
+        private string Modelo { get; set; }
+        private float Km { get; set; }
+        private decimal Precio { get; set; }
+
+        public Coche(int id, string marca, string modelo, float km, decimal precio)
+        {
+            this.Id = id;
+            this.Marca = marca;
+            this.Modelo = modelo;
+            this.Km = km;
+            this.Precio = precio;
+
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Marca:{0} Modelo:{1} Id:{2} Km:{3} Precio:{4}",Id,Marca,Modelo,Km,Precio);
+        }
+    }
+
+}
---
+++ ejercicioCoches/ejercicioCoches/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicioCoches
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int id;
+            string marca;
+            string modelo;
+            float km;
+            decimal precio;
+
+            Console.WriteLine("Ingrese id");
+            id = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese marca");
+            marca= Console.ReadLine();
+
+            Console.WriteLine("Ingrese modelo");
+            modelo= Console.ReadLine();
+
+            Console.WriteLine("Ingrese km");
+            km=float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese precio");
+            precio=decimal.Parse(Console.ReadLine());
+
+            Coche cocheUno = new Coche(id,marca,modelo,km,precio);
+
+            Console.WriteLine(cocheUno.ToString());
+
+
+        }
+    }
+}
